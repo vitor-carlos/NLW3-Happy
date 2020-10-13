3a4667a326c2244017a53e7af6654e07cea14c21
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-10-13 03:43:48 -0300

    dia 1 1.3 insert page orphanagesmap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
-import {FiArrowRight} from 'react-icons/fi'
+
 
 import'./styles/global.css';
-import'./styles/pages/landing.css';
 
-import logoImg from './images/Logo.svg';
+import Routes from './routes';
 
-interface TitleProps{
-  text:string;
-}
+
+
 function App() {
   return (
-    <div id="page-landing">
-<div className="content-wrapper">
-  <img src={logoImg} alt="Happy"/>
-<main>
-  <h1>Leve felicidade para o Mundo</h1>
-  <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-<div className="location">
-  <strong>Guarujá</strong>
-  <span>São Paulo</span>
-</div>
-<a href=""className="enter-app">
-  <FiArrowRight size={26} color="rgba(0, 0, 0.6)"/>
-</a>
-</main>
-
-</div>
-    </div>
+<Routes/>
   );
 }
 
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function OrphanagesMap(){
+return(
+  <h1>Hello World rapa</h1>
+);
+}
+export default OrphanagesMap;
