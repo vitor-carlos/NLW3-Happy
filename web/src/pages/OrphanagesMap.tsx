ba30225a50aa1449c7adcbff9b0d342098d7c620
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-10-13 04:15:32 -0300

    dia 1 1.4 insert css orphanagesmap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {FiPlus} from "react-icons/fi";
+import { Map } from 'react-leaflet';
+
+
+import mapMarkertImg from '../images/map-marker.svg';
+//import { FiPrinter } from 'react-icons/fi';//
+
+import'../styles/pages/orphanages-map.css';
+
 
 function OrphanagesMap(){
 return(
-  <h1>Hello World rapa</h1>
+  <div id="page-map">
+    <aside>
+      <header>
+        <img src={mapMarkertImg} alt="Happy"/>
+      <h2>Escolha um orfanato no mapa</h2>
+      <p>Muitas crianças estão esperando a sua visita :)</p>
+      </header>
+      <footer>
+        <strong>Guarujá</strong>
+        <span>São Paulo</span>
+      </footer>
+    </aside>
+
+
+    <div></div>
+<Link to="" className="create-orphanage">
+<FiPlus size={32} color="#fff" />
+</Link>
+
+  </div>
+
 );
 }
 export default OrphanagesMap;
