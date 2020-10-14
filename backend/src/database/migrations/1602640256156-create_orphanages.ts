8d24cf221a1400e810a89c6ffc7ff00f48fb822e
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-10-13 23:22:34 -0300

    dia 2.1 create banco

---
+++ backend/src/database/migrations/1602640256156-create_orphanages.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1602640256156 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+      await queryRunner.createTable(new Table({
+          name:'orphanages',
+          columns: [
+              {
+                  name:'id',
+                  type:'integer',
+                  unsigned:true,
+                  isPrimary:true,
+                  isGenerated:true,
+                  generationStrategy:'increment',
+
+              },
+              {
+                  name:'name',
+                  type:'varchar',
+              },
+              {
+                  name:'latitude',
+                  type:'decimal',
+                  scale:10,
+                  precision:2,
+              },
+              {
+                name:'longitude',
+                type:'decimal',
+                scale:10,
+                precision:2,
+            },
+            {
+                name:'about',
+                type:'text',
+            },
+            {
+                name:'instructions',
+                type:'text',
+            },
+            {
+                name:'open_on_weekends',
+                type:'boolean',
+                default:false,
+            },
+          ]
+      }))
+        //realizar alteracoes
+    }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+        //desfazer alteracoes
+        await queryRunner.dropTable('orphanages')
+    }
+
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,20 @@
 import express from 'express';
 
+
+import'./database/connection'
+
 const app = express();
 
-
-//rota= Conjunto
-//Recurso=usuario
-//Metodo http= GET, POST, PUT, Delete 
-
-//GET busca informacao
-//POST cria informacao
-//PUT Edita Infomacao
-//Delete informacao
-//Parametro
+app.use(express.json());
 
 
-app.post('/users', (request, response) => {
-  console.log(request.query);
+app.get('/users/:id', (request, response) => {
 
-  
   return response.json({ message: 'Hello World' });
   //console.log('teste');
 });
 
-
 app.listen(3333);
 // localhost:3333
+
 
