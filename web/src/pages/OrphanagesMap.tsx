986c4a274055973e831a4cc3462eb749f5456fc8
vitorcrl <vitor.carlos_souza@hotmail.com>
2020-10-15 04:35:22 -0300

    dia 3.5 add relationship from back and front

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect , useState} from 'react';
 import { Link } from 'react-router-dom';
 import {FiArrowRight, FiPlus} from "react-icons/fi";
 import { Map,TileLayer, Marker, Popup } from 'react-leaflet';
@@ -7,12 +7,30 @@ import { Map,TileLayer, Marker, Popup } from 'react-leaflet';
 
 import mapMarkertImg from '../images/map-marker.svg';
 //import { FiPrinter } from 'react-icons/fi';//
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
 import'../styles/pages/orphanages-map.css';
-import mapIcon from '../utils/mapIcon';
 
+
+interface Orphanage {
+  id: number;
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 function OrphanagesMap(){
+ const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+
+useEffect(() => {
+  api.get('orphanages').then(response => {
+   setOrphanages(response.data);
+  }) 
+}, []);
+
+
 return(
   <div id="page-map">
     <aside>
@@ -35,18 +53,24 @@ return(
   url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
 
 
-
-<Marker 
+{orphanages.map(orphanage =>{
+  return(
+    <Marker 
+key={orphanage.id}
 icon={mapIcon}
-position={[-23.9823302,-46.2187237]}
+position={[orphanage.latitude, orphanage.longitude]}
+
 >
   <Popup clssButton={false} minWidth={240} maxWidth={240} className="map-popup">
-Lar das Meninas
-<Link to="/orphanages/1">
+{orphanage.name}
+<Link to={`/orphanages/${orphanage.id}`}>
   <FiArrowRight size={20} color="#fff"/>
 </Link>
   </Popup>
 </Marker>
+  )
+})}
+
 </Map>
 
 
